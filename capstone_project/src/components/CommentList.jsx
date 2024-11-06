64829f8d3f634afa5cf8042aa0a14dbbfa94866c
Esteban <estebanespana97@gmail.com>
2024-11-05 16:07:37 -0800

    latest changes

--- capstone_project/src/components/CommentList.jsx
+++ capstone_project/src/components/CommentList.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { IoIosSend  } from "react-icons/io";
+import { IoPeopleSharp } from "react-icons/io5";
+
 
 const CommentList = ({ currentComments }) => {
   //this will be a list of comments
@@ -33,14 +36,17 @@ const CommentList = ({ currentComments }) => {
             ))
           :<p className="empty-message">No comments!</p>}
           <div className="chat-input-container">
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder="Type a message..."
-          className="chat-input"
-        />
-        <button onClick={handleSendMessage} className="send-button">Send</button>
+            <div className="flex9">
+              <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Type a message..."
+            className="chat-input"
+          />
+          </div>
+        <div className="flex1"></div>
+        <button onClick={handleSendMessage} id="send-button">Send</button>
       </div>
           
       </div>
